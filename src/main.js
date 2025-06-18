e600624d2202c9385350b8f842ae5384bd4ac9ad
MARCOGN43 <marcoliviergn@gmail.com>
2025-06-18 01:11:49 +0100

    style: update unocss utilisation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+import { mount } from 'svelte'
+import App from './App.svelte'
+
+const app = mount(App, {
+  target: document.getElementById('app'),
+})
+
+export default app
+
+// add unocss
+
 import 'virtual:uno.css'
