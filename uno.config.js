14bfd447bc853d5cf4b4a2be2a6229c49ce4ed59
MARCOGN43 <marcoliviergn@gmail.com>
2025-06-18 01:04:09 +0100

    chore: add presets to UnoCSS

--- uno.config.js
+++ uno.config.js
@@ -1,5 +1,15 @@
-import { defineConfig } from 'unocss'
+import { defineConfig } from "unocss";
+import presetWind4 from "@unocss/preset-wind4";
+import { presetWebFonts } from "unocss";
 
 export default defineConfig({
-  // ...UnoCSS options
-})
+  presets: [
+    presetWind4(),
+    presetWebFonts({
+      provider: "fontshare",
+      fonts: {
+        sans: "Hind",
+      },
+    }),
+  ],
+});
